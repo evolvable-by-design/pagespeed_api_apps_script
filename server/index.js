3dceefd15a86fe6461ee9436938dcae26df48460
Antoine Cheron <cheron.antoine@gmail.com>
2020-09-08 15:59:25 +0200

    Ease version switching

--- server/index.js
+++ server/index.js
@@ -4,45 +4,55 @@ const path = require('path')
 const YAML = require('yamljs')
 
 const PORT = process.env.PORT || 8000
-
-const apiDocumentation = YAML.load(path.join(__dirname, '/openapi.yaml'))
+const VERSION = 2 // 1 or 2
+const DOCUMENTATION_FILE_NAME = VERSION === 1 ? '/openapi-v1.yaml' : '/openapi-v2.yaml'
+const apiDocumentation = YAML.load(path.join(__dirname, DOCUMENTATION_FILE_NAME))
 
 const app = express()
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.options('/', (_, res) => res.status(200).json(apiDocumentation))
 
 app.get('/v1/runPageSpeed', (req, res) => {
-  const { url, filter_third_party_resources, strategy, key } = req.query
+  if (VERSION === 1) {
+    const { url, filter_third_party_resources, strategy, key } = req.query
 
-  if ([url, filter_third_party_resources, strategy, key].includes(undefined)) {
-    res.status(400).json({ errorMessage: 'Incorrect request body' })
+    if ([url, filter_third_party_resources, strategy, key].includes(undefined)) {
+      res.status(400).json({ errorMessage: 'Incorrect request body' })
+    }
+
+    res.status(200).json({
+      kind: 'pagespeedonline#result',
+      id: url,
+      responseCode: 200,
+      score: 80
+    })
+  } else {
+    res.set('Location', 'https://www.googleapis.com/pagespeedonline/v2/runPageSpeed').sendStatus(301)
   }
-
-  res.status(200).json({
-    kind: 'pagespeedonline#result',
-    id: url,
-    responseCode: 200,
-    score: 80
-  })
 })
 
 app.get('/v2/runPageSpeed', (req, res) => {
-  const { url, filter_third_party_resources, strategy, key } = req.query
+  if (VERSION === 2) {
+    const { url, filter_third_party_resources, strategy, key } = req.query
 
-  if ([url, filter_third_party_resources, strategy, key].includes(undefined)) {
-    res.status(400).json({ errorMessage: 'Incorrect request body' })
-  }
-
-  res.status(200).json({
-    kind: 'pagespeedonline#result',
-    id: url,
-    responseCode: 200,
-    ruleGroups: {
-      SPEED: {
-        score: 80
-      }
+    if ([url, filter_third_party_resources, strategy, key].includes(undefined)) {
+      res.status(400).json({ errorMessage: 'Incorrect request body' })
     }
-  })
+
+    res.status(200).json({
+      kind: 'pagespeedonline#result',
+      id: url,
+      responseCode: 200,
+      ruleGroups: {
+        score: 70,
+        SPEED: {
+          score: 70
+        }
+      }
+    })
+  } else {
+    res.sendStatus(404)
+  }
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
